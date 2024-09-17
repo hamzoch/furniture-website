2c49da8214cf13aeb33020ed046fbc8d7db60c78
hamzoch <hamzadaqare@gmail.com>
2024-09-17 07:29:33 -0500

    added javascript

---
+++ script.js
@@ -0,0 +1,6 @@
+const showmenu = document.getElementById("showmenu");
+const list = document.getElementById("list");
+showmenu.addEventListener('click', ()=>{
+list.classList.toggle("hidemenu")
+
+})
